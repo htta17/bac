eab7c67e3dfc7513f3b2ce9d814ce7205e7accd4
Anh Hoang <htta17@gmail.com>
2022-03-09 22:21:19 -0600

    database

--- Baccarat/SQL/New database.sql
+++ Baccarat/SQL/New database.sql
@@ -1,4 +1,4 @@
-﻿
+﻿/****** Object:  Table [dbo].[AutoResults]    Script Date: 3/9/2022 10:08:36 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -8,15 +8,14 @@ CREATE TABLE [dbo].[AutoResults](
 	[Card] [smallint] NOT NULL,
 	[InputDateTime] [datetime] NOT NULL,
 	[AutoSessionID] [int] NOT NULL,
-	[UIResult] [nvarchar](100) NULL,
+	[UIResult] [nvarchar](200) NULL,
  CONSTRAINT [PK_dbo.StandardResults] PRIMARY KEY CLUSTERED 
 (
 	[ID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
-
 GO
-/****** Object:  Table [dbo].[AutoRoots]    Script Date: 3/8/2022 12:10:35 AM ******/
+/****** Object:  Table [dbo].[AutoRoots]    Script Date: 3/9/2022 10:08:36 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -33,22 +32,21 @@ CREATE TABLE [dbo].[AutoRoots](
 	[Profit2] [int] NOT NULL,
 	[Profit3] [int] NOT NULL,
 	[AllSubProfit] [int] NOT NULL,
-	[ModMainProfit] [int]  NULL,
-	[ModProfit0] [int]  NULL,
-	[ModProfit1] [int]  NULL,
-	[ModProfit2] [int]  NULL,
-	[ModProfit3] [int]  NULL,
-	[ModAllSubProfit] [int]  NULL,
+	[ModMainProfit] [int] NULL,
+	[ModProfit0] [int] NULL,
+	[ModProfit1] [int] NULL,
+	[ModProfit2] [int] NULL,
+	[ModProfit3] [int] NULL,
+	[ModAllSubProfit] [int] NULL,
 	[ListCurrentPredicts] [nvarchar](500) NOT NULL,
 	[ListCurrentModCoeffs] [nvarchar](500) NOT NULL,
  CONSTRAINT [PK_dbo.StandardRoots] PRIMARY KEY CLUSTERED 
 (
 	[ID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
-
 GO
-/****** Object:  Table [dbo].[AutoSessions]    Script Date: 3/8/2022 12:10:35 AM ******/
+/****** Object:  Table [dbo].[AutoSessions]    Script Date: 3/9/2022 10:08:36 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -82,11 +80,10 @@ CREATE TABLE [dbo].[AutoSessions](
  CONSTRAINT [PK_dbo.StandardSessions] PRIMARY KEY CLUSTERED 
 (
 	[ID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
-
 GO
-/****** Object:  Table [dbo].[Results]    Script Date: 3/8/2022 12:10:35 AM ******/
+/****** Object:  Table [dbo].[Results]    Script Date: 3/9/2022 10:08:36 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -99,11 +96,10 @@ CREATE TABLE [dbo].[Results](
  CONSTRAINT [PK_dbo.Results] PRIMARY KEY CLUSTERED 
 (
 	[ID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
-
 GO
-/****** Object:  Table [dbo].[Roots]    Script Date: 3/8/2022 12:10:35 AM ******/
+/****** Object:  Table [dbo].[Roots]    Script Date: 3/9/2022 10:08:36 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -154,11 +150,10 @@ CREATE TABLE [dbo].[Roots](
  CONSTRAINT [PK_dbo.Roots] PRIMARY KEY CLUSTERED 
 (
 	[ID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
-
 GO
-/****** Object:  Table [dbo].[Sessions]    Script Date: 3/8/2022 12:10:35 AM ******/
+/****** Object:  Table [dbo].[Sessions]    Script Date: 3/9/2022 10:08:36 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -179,9 +174,8 @@ CREATE TABLE [dbo].[Sessions](
  CONSTRAINT [PK_dbo.Sessions] PRIMARY KEY CLUSTERED 
 (
 	[ID] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
 ) ON [PRIMARY]
-
 GO
 ALTER TABLE [dbo].[Roots] ADD  DEFAULT ((0)) FOR [Flat095Main]
 GO
@@ -237,31 +231,53 @@ ON DELETE CASCADE
 GO
 ALTER TABLE [dbo].[Results] CHECK CONSTRAINT [FK_dbo.Results_dbo.Sessions_SessionID]
 GO
-/****** Object:  StoredProcedure [dbo].[CommonQuery]    Script Date: 3/8/2022 12:10:35 AM ******/
+/****** Object:  StoredProcedure [dbo].[CommonQuery]    Script Date: 3/9/2022 10:08:36 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-
 CREATE PROCEDURE [dbo].[CommonQuery]
 	-- Add the parameters for the stored procedure here
 	@tablenumber int
 AS
 BEGIN
 
-select ID, TableNumber, NoOfStepsRoot, StartDateTime,MinRoot, MaxRoot, RootMainProfit + RootAllSub * 2 AS GlobalProfit 
-from AutoSessions 
+	select SUM(TotalProfit) AS LifeTimeTotal, SUM(RootMainProfit) AS LifeTimeMain
+	FROM 
+	(
+		select TableNumber, IsClosed, NoOfStepsRoot, StartDateTime,MinRoot, MaxRoot, RootMainProfit + RootAllSub * 2 AS TotalProfit, RootMainProfit  
+		from AutoSessions 
+	) AS A
 
-select SUM(TotalProfit) AS LifeTimeTotal, SUM(RootMainProfit) AS LifeTimeMain
-FROM 
-(
-	select TableNumber, IsClosed, NoOfStepsRoot, StartDateTime,MinRoot, MaxRoot, RootMainProfit + RootAllSub * 2 AS TotalProfit, RootMainProfit  
+	select TableNumber, SUM( RootMainProfit + RootAllSub) GB1, SUM( RootMainProfit + RootAllSub * 2) AS LifeTimeTotal, SUM( RootMainProfit) AS LifeTimeMain
+	 from AutoSessions GROUP BY TableNumber
+
+	select StartDateTime, TableNumber,  RootMainProfit , RootAllSub + RootMainProfit * 2 AS 'Main, 2 * (all subs)', IsClosed from AutoSessions
+	where TableNumber = 1
+
+	select StartDateTime, TableNumber,  RootMainProfit , RootAllSub + RootMainProfit * 2 AS  'Main, 2 * (all subs)', IsClosed from AutoSessions
+	where TableNumber = 2
+
+	select StartDateTime, TableNumber,  RootMainProfit , RootAllSub + RootMainProfit * 2 AS  'Main, 2 * (all subs)', IsClosed from AutoSessions
+	where TableNumber = 3
+
+	select StartDateTime, TableNumber,  RootMainProfit , RootAllSub + RootMainProfit * 2 AS  'Main, 2 * (all subs)', IsClosed from AutoSessions
+	where TableNumber = 4
+
+	select StartDateTime, TableNumber,  RootMainProfit , RootAllSub + RootMainProfit * 2 AS  'Main, 2 * (all subs)', IsClosed from AutoSessions
+	where TableNumber = 5
+
+	select StartDateTime, TableNumber,  RootMainProfit , RootAllSub + RootMainProfit * 2 AS  'Main, 2 * (all subs)', IsClosed  from AutoSessions
+	where TableNumber = 6
+
+	select StartDateTime, TableNumber,  RootMainProfit , RootAllSub + RootMainProfit * 2 AS  'Main, 2 * (all subs)', IsClosed  from AutoSessions
+	where TableNumber = 7
+
+	select ID, TableNumber, NoOfStepsRoot, StartDateTime,MinRoot, MaxRoot,RootMainProfit, RootMainProfit + RootAllSub * 2 AS GlobalProfit ,  
+			CASE ISCLOSED WHEN 1 THEN 'Closed' ELSE 'Open' END [Status]	
 	from AutoSessions 
-) AS A
 
-select TableNumber, SUM( RootMainProfit + RootAllSub * 2) AS LifeTimeTotal, SUM( RootMainProfit) AS LifeTimeMain
- from AutoSessions 
-GROUP BY TableNumber
+
+
 END
-
 GO
